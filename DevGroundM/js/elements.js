aaf89044c1db47fa1aa9fc163b9a582e53eb3ef1
Onyelechie <onyelechieebere@gmail.com>
2023-09-22 08:05:15 -0500

    Changes made to the dev branch. Mission menu created with pill type buttons Floating div created for containing -latitiude -longitude -altitude (colour would be changed by next commit/ pull request) Waypoint abstract data structure class created -uses a linkedlist implementation

--- DevGroundM/js/elements.js
+++ DevGroundM/js/elements.js
@@ -16,6 +16,10 @@ document.getElementById("stats-select").addEventListener("click", function () {
     utilitySwitcher(1);
 });
 
+document.getElementById("mission-select").addEventListener("click", function () {
+    utilitySwitcher(5);
+});
+
 document.getElementById("emi-mpopup").addEventListener("click", function () {
     var menu = document.getElementById("ECV-menu");
     menu.style.visibility = "visible";
@@ -79,12 +83,25 @@ function showEMIMenu(){
     smnu.style.visibility = "visible";
 }
 
+function showMissionMenu(){
+    var smnu = document.getElementById("mission-menu");
+    //Set no visibility
+    smnu.style.visibility = "visible";
+}
+
+function hideMissionMenu(){
+    var smnu = document.getElementById("mission-menu");
+    //Set no visibility
+    smnu.style.visibility = "hidden";
+}
+
 function utilitySwitcher(pageFlag){
     /*
         1 -> Stats Menu
         2 -> Actions Menu
         3 -> Logs Menu
         4 -> EMI Menu
+        5 -> Mission Menu
     */
    //Hide previous page
    switch(cPage){
@@ -103,6 +120,10 @@ function utilitySwitcher(pageFlag){
     case 4:
         hideEMIMenu();
         document.getElementById("emi-select").className = "nonactive";
+        break;
+    case 5:
+        hideMissionMenu();
+        document.getElementById("mission-select").className = "nonactive";
         break;
 }
    //Switch current page flag
@@ -123,8 +144,118 @@ function utilitySwitcher(pageFlag){
             showEMIMenu();
             document.getElementById("emi-select").className = "active";
             break;
+        case 5:
+            showMissionMenu();
+            document.getElementById("mission-select").className = "active";
+            break;    
     }
     //Set new current page
     cPage = pageFlag;
 }
 
+var dragItem = document.querySelector("#wayPointContainer");
+var container = document.querySelector("#mission-menu");
+
+var active = false;
+var currentX;
+var currentY;
+var initialX;
+var initialY;
+var xOffset = 0;
+var yOffset = 0;
+
+container.addEventListener("touchstart", dragStart, false);
+container.addEventListener("touchend", dragEnd, false);
+container.addEventListener("touchmove", drag, false);
+
+container.addEventListener("mousedown", dragStart, false);
+container.addEventListener("mouseup", dragEnd, false);
+container.addEventListener("mousemove", drag, false);
+
+function dragStart(e) {
+  if (e.type === "touchstart") {
+    initialX = e.touches[0].clientX - xOffset;
+    initialY = e.touches[0].clientY - yOffset;
+  } else {
+    initialX = e.clientX - xOffset;
+    initialY = e.clientY - yOffset;
+  }
+
+  if (e.target === dragItem) {
+    active = true;
+  }
+}
+
+function dragEnd(e) {
+  initialX = currentX;
+  initialY = currentY;
+
+  active = false;
+}
+
+function drag(e) {
+  if (active) {
+  
+    e.preventDefault();
+  
+    if (e.type === "touchmove") {
+      currentX = e.touches[0].clientX - initialX;
+      currentY = e.touches[0].clientY - initialY;
+    } else {
+      currentX = e.clientX - initialX;
+      currentY = e.clientY - initialY;
+    }
+
+    xOffset = currentX;
+    yOffset = currentY;
+
+    setTranslate(currentX, currentY, dragItem);
+  }
+}
+
+function setTranslate(xPos, yPos, el) {
+  el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+}
+
+
+class Node{
+    constructor(latitude, longitude, altitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
+        this.next = null;
+    }
+}
+
+class Waypoint{
+    constructor(){
+        this.top = null;
+        this.tail =null;
+        this.id = 0;
+    }
+
+     addWayPoint(latitude,longitude,altitude){
+        newNode = new Node(latitude,longitude,altitude);
+
+        if(this.top == null){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail= newNode;
+        }
+        id++;
+    }
+
+    deleteWayPoint(id){
+    tempNode = top;
+    parentNode = null;
+    while(tempNode != null){
+        if(tempNode.id == id){
+            parentNode.next = tempNode.next;
+        }
+        parentNode = tempNode;
+        tempNode = tempNode.next;
+    }
+    }
+}
