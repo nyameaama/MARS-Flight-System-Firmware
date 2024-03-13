adf5d0d34fab627846e9c680a6ad74d6aaad2b9b
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2024-03-13 14:35:38 -0400

    small change to bluetooth service+characteristics

---
+++ test/gatt_server_service_table/main/bt_data.c
@@ -0,0 +1,81 @@
+#include "bt_data.h"
+
+char* readDataCharacteristic1(void) {
+    return "Data not implemented - Characteristic 1";
+}
+
+char* readDataCharacteristic2(void) {
+    return "Data not implemented - Characteristic 2";
+}
+
+char* readDataCharacteristic3(void) {
+    return "Data not implemented- Characteristic 3";
+}
+
+char* readDataCharacteristic4(void) {
+    return "Data not implemented- Characteristic 4";
+}
+
+char* readDataCharacteristic5(void) {
+    return "Data not implemented- Characteristic 5";
+}
+
+char* readDataCharacteristic6(void) {
+    return "Data not implemented- Characteristic 6";
+}
+
+char* readDataCharacteristic7(void) {
+    return "Data not implemented- Characteristic 7";
+}
+
+char* readDataCharacteristic8(void) {
+    return "Data not implemented- Characteristic 8";
+}
+
+char* readDataCharacteristic9(void) {
+    return "Data not implemented- Characteristic 9";
+}
+
+char* readDataCharacteristic10(void) {
+    return "Data not implemented- Characteristic 10";
+}
+
+char* readDataCharacteristic11(void) {
+    return "Data not implemented- Characteristic 11";
+}
+
+char* readDataCharacteristic12(void) {
+    return "Data not implemented- Characteristic 12";
+}
+
+char* readDataCharacteristic13(void) {
+    return "Data not implemented- Characteristic 13";
+}
+
+char* readDataCharacteristic14(void) {
+    return "Data not implemented- Characteristic 14";
+}
+
+char* readDataCharacteristic15(void) {
+    return "Data not implemented- Characteristic 15";
+}
+
+char* readDataCharacteristic16(void) {
+    return "Data not implemented";
+}
+
+char* readDataCharacteristic17(void) {
+    return "Data not implemented";
+}
+
+char* readDataCharacteristic18(void) {
+    return "Data not implemented";
+}
+
+char* readDataCharacteristic19(void) {
+    return "Data not implemented";
+}
+
+char* readDataCharacteristic20(void) {
+    return "Data not implemented";
+}
---
+++ test/gatt_server_service_table/main/bt_data.h
@@ -0,0 +1,25 @@
+#ifndef READ_CHARACTERISTICS_H
+#define READ_CHARACTERISTICS_H
+
+char* readDataCharacteristic1(void);
+char* readDataCharacteristic2(void);
+char* readDataCharacteristic3(void);
+char* readDataCharacteristic4(void);
+char* readDataCharacteristic5(void);
+char* readDataCharacteristic6(void);
+char* readDataCharacteristic7(void);
+char* readDataCharacteristic8(void);
+char* readDataCharacteristic9(void);
+char* readDataCharacteristic10(void);
+char* readDataCharacteristic11(void);
+char* readDataCharacteristic12(void);
+char* readDataCharacteristic13(void);
+char* readDataCharacteristic14(void);
+char* readDataCharacteristic15(void);
+char* readDataCharacteristic16(void);
+char* readDataCharacteristic17(void);
+char* readDataCharacteristic18(void);
+char* readDataCharacteristic19(void);
+char* readDataCharacteristic20(void);
+
+#endif // READ_CHARACTERISTICS_H
